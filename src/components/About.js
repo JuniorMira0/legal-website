295c7d4a67da565c3a9d191b4d6ee59d6d76087a
JuniorMira0 <j-unyor14@hotmail.com>
2025-04-16 23:00:37 -0300

    style: update About and FeatureSection1 styles for improved layout and readability; adjust text sizes and margins

--- src/components/About.js
+++ src/components/About.js
@@ -16,16 +16,14 @@ function About() {
           <p>
             Guilherme Quintiliano Rehme (OAB/PR 111.012), advogado formado pela
             Unicuritiba, atua em Curitiba-PR desde 2018 com foco em{' '}
-            <strong className="destaque-importante">
-              Direito do Consumidor
-            </strong>{' '}
-            e <strong className="destaque-importante">Direito Civil</strong>.
-            Com experiência prática em diversas fases processuais e sistemas
-            judiciais, busca oferecer soluções jurídicas eficientes e
-            personalizadas em todos os estados. Sua prática abrange também em
-            casos específicos relacionados a Viagens. Conte com a sua dedicação
-            e preparo para receber a melhor assistência jurídica, especialmente
-            nas áreas que mais impactam o seu dia a dia.
+            <strong>Direito do Consumidor</strong> e{' '}
+            <strong>Direito Civil</strong>. Com experiência prática em diversas
+            fases processuais e sistemas judiciais, busca oferecer soluções
+            jurídicas eficientes e personalizadas em todos os estados. Sua
+            prática abrange também em casos específicos relacionados a Viagens.
+            Conte com a sua dedicação e preparo para receber a melhor
+            assistência jurídica, especialmente nas áreas que mais impactam o
+            seu dia a dia.
           </p>
         </div>
         <div className="about-image-container">
