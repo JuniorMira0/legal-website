f600d7ebf58c73af8c85b61909b4e97d07795357
JuniorMira0 <j-unyor14@hotmail.com>
2025-04-22 15:51:43 -0300

    feat: implement form submission functionality in ContactSection component

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,16 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ContactSection.css';
 import { FaWhatsapp } from 'react-icons/fa';
 import contactImage from '../assets/images/contact.png';
 
 function ContactSection() {
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    alert('Funcionalidade de envio de formulário a ser implementada!');
-  };
-
   const whatsappLink =
     'https://wa.me/554188050966?text=Olá,%20encontrei%20seu%20contato%20pelo%20site%20e%20gostaria%20de%20falar%20com%20o%20advogado%20Guilherme%20Quintiliano.%20Pode%20me%20ajudar?';
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [whatsappNumber, setWhatsappNumber] = useState('');
+  const [status, setStatus] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setStatus('Enviando...');
+
+    const form = event.target;
+    const data = {
+      name,
+      email,
+      message,
+      'whatsapp-number': whatsappNumber,
+    };
+
+    try {
+      const response = await fetch(form.action, {
+        method: form.method,
+        body: JSON.stringify(data),
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        setName('');
+        setEmail('');
+        setMessage('');
+        setWhatsappNumber('');
+        setStatus('Mensagem enviada com sucesso!');
+      } else {
+        const errorData = await response.json();
+        if (Object.hasOwn(errorData, 'errors')) {
+          setStatus(
+            `Erro: ${errorData['errors']
+              .map((error) => error['message'])
+              .join(', ')}`,
+          );
+        } else {
+          setStatus('Oops! Ocorreu um erro ao enviar a mensagem.');
+        }
+      }
+    } catch (error) {
+      setStatus('Oops! Ocorreu um erro de rede ao enviar a mensagem.');
+    }
+  };
 
   return (
     <section id="contato" className="contact-section section">
@@ -23,7 +69,12 @@ function ContactSection() {
 
         <div className="contact-layout-wrapper">
           <div className="contact-form-column">
-            <form className="contact-form" onSubmit={handleFormSubmit}>
+            <form
+              className="contact-form"
+              action="https://formspree.io/f/meogqedr"
+              method="POST"
+              onSubmit={handleSubmit}
+            >
               <div className="form-group">
                 <label htmlFor="name">Nome</label>
                 <input
@@ -33,6 +84,8 @@ function ContactSection() {
                   placeholder="Digite seu nome"
                   required
                   autoComplete="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                 />
               </div>
               <div className="form-group">
@@ -44,6 +97,8 @@ function ContactSection() {
                   placeholder="Digite seu email"
                   required
                   autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div className="form-group">
@@ -55,6 +110,8 @@ function ContactSection() {
                   placeholder="Escreva sua mensagem"
                   required
                   autoComplete="off"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
                 ></textarea>
               </div>
               <div className="form-group">
@@ -65,8 +122,13 @@ function ContactSection() {
                   name="whatsapp-number"
                   placeholder="Seu número WhatsApp"
                   autoComplete="tel"
+                  value={whatsappNumber}
+                  onChange={(e) => setWhatsappNumber(e.target.value)}
                 />
               </div>
+
+              {status && <p className="form-status">{status}</p>}
+
               <div className="form-button-group">
                 <button type="submit" className="submit-button">
                   Enviar Mensagem
